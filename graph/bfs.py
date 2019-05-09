1671ac7ac543eab7cfc1efafe4696285fc9e250d
madsh01 <madsh01@ca.com>
2019-05-09 21:48:27 +0530

    bfs algorithm

---
+++ graph/bfs.py
@@ -0,0 +1,36 @@
+from queue import Queue
+from graph import *
+
+
+def bfs(graph, start=0):
+    queue = Queue()
+    queue.put(start)
+    visited = set()
+    while not queue.empty():
+        node = queue.get()
+
+        if node in visited:
+            continue
+
+        print("Vertex", node)
+        visited.add(node)
+
+        for v in graph.get_adjacent_vertex(node):
+            if v not in visited:
+                queue.put(v)
+
+
+
+g = AdjacencyMatrixGraph(6,False)
+
+g.add_edge(0,1)
+g.add_edge(0,2)
+g.add_edge(2,3)
+g.add_edge(2,1)
+g.add_edge(1,4)
+g.add_edge(4,5)
+g.add_edge(3,5)
+
+
+g.display()
+bfs(g,5)
